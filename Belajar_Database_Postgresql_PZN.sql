9ed1157b9af385a4b9e1ce5563f687787a4aa7d1
projekbrillylutfan <projekbrillylutfan@gmail.com>
2024-10-21 14:51:38 +0700

    alamak

--- Belajar_Database_Postgresql_PZN.sql
+++ Belajar_Database_Postgresql_PZN.sql
@@ -48,3 +48,155 @@ SELECT id, name, price, quantity from products;
 
 alter table products
     add PRIMARY KEY (id);
+
+select id, name, price, quantity
+from products
+where quantity = 0;
+
+select id, name, price, quantity
+from products
+where price = 20000;
+
+select id, name, price, quantity
+from products
+where id = 'P0004';
+
+CREATE TYPE PRODUCT_CATEGORY AS ENUM ('Makanan', 'Minuman', 'Lain-lain');
+
+ALTER Table products
+    add column category PRODUCT_CATEGORY;
+
+update products
+set category = 'Makanan'
+where id = 'P0001';
+
+SELECT * FROM products;
+
+update products
+set category = 'Makanan'
+where id = 'P0002';
+
+update products
+set category = 'Makanan'
+where id = 'P0003';
+
+update products
+set category = 'Makanan'
+where id = 'P0004';
+
+update products
+set category = 'Makanan'
+where id = 'P0005';
+
+update products
+set category    = 'Makanan',
+    description = 'Mie Ayam + Ceker'
+where id = 'P0003';
+
+update products
+set price = price + 5000
+where id = 'P0004';
+
+SELECT * FROM products;
+
+insert into products(id, name, price, quantity, category)
+values ('P0009', 'Contoh', 10000, 100, 'Minuman');
+
+delete
+from products
+where id = 'P0009';
+
+select id as "Kode Barang", price as "Harga Barang", description as "Deskripsi Barang"
+from products;
+
+select p.id          as "Kode Barang",
+       p.price       as "Harga Barang",
+       p.description as "Deskripsi Barang"
+from products as p;
+
+select *
+from products
+where price > 15000;
+
+select *
+from products
+where price <= 15000;
+
+select *
+from products
+where category != 'Minuman';
+
+select *
+from products
+where price > 15000
+  and category = 'Makanan';
+
+SELECT *
+FROM products;
+
+insert into products(id, name, price, quantity, category)
+values ('P0006', 'Es teh tawar', 10000, 100, 'Minuman'),
+       ('P0007', 'Es Campur', 20000, 100, 'Minuman'),
+       ('P0008', 'Just Jeruk', 15000, 100, 'Minuman');
+
+select *
+from products
+where price > 15000
+   or category = 'Makanan';
+
+select *
+from products
+where quantity > 100
+   OR category = 'Makanan' and price > 15000;
+
+select *
+from products
+where category = 'Makanan'
+   or (quantity > 100 and price > 15000);
+
+select *
+from products
+where name ilike '%es%';
+
+select *
+from products
+where description is null;
+
+select *
+from products
+where description is not null;
+
+select *
+from products
+where price between 10000 and 20000;
+
+select *
+from products
+where price not between 10000 and 20000;
+
+select *
+from products
+where category in ('Makanan', 'Minuman');
+
+select *
+from products
+order by price asc, id desc;
+
+select *
+from products
+where price > 0
+order by price asc, id desc;
+
+select *
+from products
+where price > 0
+order by price asc, id desc
+limit 2;
+
+-- 1 limit 2 offset 0, 2 limit 2 offset 2, 3 limit 2 offset 4
+
+select *
+from products
+where price > 0
+order by price asc, id desc
+limit 2 offset 2;
