dcb63880b5491a60105a4603e69bf82bde74f814
projekbrillylutfan <projekbrillylutfan@gmail.com>
2024-10-21 21:12:04 +0700

    makin menarik

--- Belajar_Database_Postgresql_PZN.sql
+++ Belajar_Database_Postgresql_PZN.sql
@@ -284,3 +284,326 @@ select id,
            else description
            end as description
 from products;
+
+-- Active: 1729346896710@@127.0.0.1@5432@belajar_database_postgresql_pzn@public
+SELECT datname from pg_database;
+
+select * from pg_tables WHERE schemaname = 'public';
+
+CREATE Table barang (
+    kode INT not NULL,
+    name VARCHAR(100) not NULL,
+    harga INT not NULL DEFAULT 1000,
+    jumlah INT not NULL DEFAULT 0,
+    waktu_dibuat TIMESTAMP not null DEFAULT CURRENT_TIMESTAMP
+);
+
+alter Table barang add column deskripsi TEXT;
+
+alter table barang DROP COLUMN deskripsi;
+
+alter table barang RENAME COLUMN name to nama;
+
+TRUNCATE barang;
+
+DROP Table barang;
+
+CREATE Table products (
+    id VARCHAR(10) not NULL,
+    name VARCHAR(100) not NULL,
+    description TEXT,
+    price int NOT NULL,
+    quantity int NOT NULL DEFAULT 0,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+INSERT INTO products(id, name, price, quantity)
+VALUES ('P0001', 'Pensil', 1000, 100);
+
+INSERT INTO products(id, name, description, price, quantity)
+VALUES ('P0002', 'Penghapus', 'Penghapus warna', 2000, 200);
+
+
+insert into products(id, name, price, quantity)
+values ('P0003', 'Mie Ayam Ceker', 20000, 100),
+       ('P0004', 'Mie Ayam Spesial', 25000, 100),
+       ('P0005', 'Mie Ayam Yamin', 15000, 100);
+
+SELECT * FROM products;
+
+SELECT id, name, price, quantity from products;
+
+alter table products
+    add PRIMARY KEY (id);
+
+select id, name, price, quantity
+from products
+where quantity = 0;
+
+select id, name, price, quantity
+from products
+where price = 20000;
+
+select id, name, price, quantity
+from products
+where id = 'P0004';
+
+CREATE TYPE PRODUCT_CATEGORY AS ENUM ('Makanan', 'Minuman', 'Lain-lain');
+
+ALTER Table products
+    add column category PRODUCT_CATEGORY;
+
+update products
+set category = 'Makanan'
+where id = 'P0001';
+
+SELECT * FROM products;
+
+update products
+set category = 'Makanan'
+where id = 'P0002';
+
+update products
+set category = 'Makanan'
+where id = 'P0003';
+
+update products
+set category = 'Makanan'
+where id = 'P0004';
+
+update products
+set category = 'Makanan'
+where id = 'P0005';
+
+update products
+set category    = 'Makanan',
+    description = 'Mie Ayam + Ceker'
+where id = 'P0003';
+
+update products
+set price = price + 5000
+where id = 'P0004';
+
+SELECT * FROM products;
+
+insert into products(id, name, price, quantity, category)
+values ('P0009', 'Contoh', 10000, 100, 'Minuman');
+
+delete
+from products
+where id = 'P0009';
+
+select id as "Kode Barang", price as "Harga Barang", description as "Deskripsi Barang"
+from products;
+
+select p.id          as "Kode Barang",
+       p.price       as "Harga Barang",
+       p.description as "Deskripsi Barang"
+from products as p;
+
+select *
+from products
+where price > 15000;
+
+select *
+from products
+where price <= 15000;
+
+select *
+from products
+where category != 'Minuman';
+
+select *
+from products
+where price > 15000
+  and category = 'Makanan';
+
+SELECT *
+FROM products;
+
+insert into products(id, name, price, quantity, category)
+values ('P0006', 'Es teh tawar', 10000, 100, 'Minuman'),
+       ('P0007', 'Es Campur', 20000, 100, 'Minuman'),
+       ('P0008', 'Just Jeruk', 15000, 100, 'Minuman');
+
+select *
+from products
+where price > 15000
+   or category = 'Makanan';
+
+select *
+from products
+where quantity > 100
+   OR category = 'Makanan' and price > 15000;
+
+select *
+from products
+where category = 'Makanan'
+   or (quantity > 100 and price > 15000);
+
+select *
+from products
+where name ilike '%es%';
+
+select *
+from products
+where description is null;
+
+select *
+from products
+where description is not null;
+
+select *
+from products
+where price between 10000 and 20000;
+
+select *
+from products
+where price not between 10000 and 20000;
+
+select *
+from products
+where category in ('Makanan', 'Minuman');
+
+select *
+from products
+order by price asc, id desc;
+
+select *
+from products
+where price > 0
+order by price asc, id desc;
+
+select *
+from products
+where price > 0
+order by price asc, id desc
+limit 2;
+
+-- 1 limit 2 offset 0, 2 limit 2 offset 2, 3 limit 2 offset 4
+
+select *
+from products
+where price > 0
+order by price asc, id desc
+limit 2 offset 2;
+
+select category
+from products;
+
+select distinct category
+from products;
+
+select 10 + 10 as hasil;
+
+select id, name, price / 1000 as price_in_k
+from products;
+
+select pi();
+
+select power(10, 2);
+
+select cos(10), sin(10), tan(10);
+
+select id, name, power(quantity, 2) as quantity_power_2
+from products;
+
+create table admin
+(
+    id         serial       not null,
+    first_name varchar(100) not null,
+    last_name  varchar(100),
+    primary key (id)
+);
+
+insert into admin(first_name, last_name)
+values ('Eko', 'Khannedy'),
+       ('Budi', 'Nugraha'),
+       ('Joko', 'Morro');
+
+select * from admin;
+
+select currval('admin_id_seq');
+
+create sequence contoh_sequence;
+
+select nextval('contoh_sequence');
+
+select currval('contoh_sequence');
+
+select id, name, description
+from products;
+
+select id, lower(name), length(name), lower(description)
+from products;
+
+select *
+from products;
+
+select id, extract(year from created_at) as Year, extract(month from created_at) as Month
+from products;
+
+select id, category
+from products;
+
+select id,
+       category,
+       case category
+           when 'Makanan' then 'Enak'
+           when 'Minuman' then 'Seger'
+           else 'Apa itu?'
+           end as category_case
+from products;
+
+select id,
+       price,
+       case
+           when price <= 15000 then 'Murah'
+           when price <= 20000 then 'Mahal'
+           else 'Mahal Banget'
+           end as "apakah murah?"
+from products;
+
+select id,
+       name,
+       case
+           when description is null then 'kosong'
+           else description
+           end as description
+from products;
+
+select count(id)
+from products;
+
+select avg(price)
+from products;
+
+select max(price)
+from products;
+
+select min(price)
+from products;
+
+select category, count(id) as "Total Product"
+from products
+group by category;
+
+select category,
+       avg(price) as "Rata Rata Harga",
+       min(price) as "Harga termurah",
+       max(price) as "Harga termahal"
+from products
+group by category;
+
+select category, count(id) as "Total Product"
+from products
+group by category
+having count(id) > 1;
+
+select category,
+       avg(price) as "Rata Rata Harga",
+       min(price) as "Harga termurah",
+       max(price) as "Harga termahal"
+from products
+group by category
+having avg(price) >= 1000;
